53f18a437e6d0e637c08fce0350541c27c037570
garvincasimir <gjcasimir@gmail.com>
2013-12-31 16:56:17 -0500

    Adding a switch for MariaDB

--- nunit.sh
+++ nunit.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+#Setup database
+if [ "$DbServer" = "MariaDB" ]; then
+   wget http://ftp.osuosl.org/pub/mariadb/mariadb-5.3.12/kvm-tarbake-jaunty-x86/mariadb-5.3.12.tar.gz
+   tar -xvzf ~/Downloads/mariadb-5.3.12.tar.gz
+   cd mariadb-5.3.12
+   ./configure 
+   make
+   sudo make install
+   cd ..
+else
+    wget http://cdn.mysql.com/Downloads/MySQL-5.1/mysql-5.1.73-osx10.6-x86_64.dmg
+    hdiutil attach -quiet "mysql-5.1.73-osx10.6-x86_64.dmg"
+    sudo installer -pkg "/Volumes/mysql-5.1.73-osx10.6-x86_64/mysql-5.1.73-osx10.6-x86_64.pkg" -target /
+    sudo /usr/local/mysql/bin/mysqld_safe &
+fi
+
 #get latest version of nuget
 curl -S -L  http://nuget.org/nuget.exe > .nuget/nuget.exe
 
